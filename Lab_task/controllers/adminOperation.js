d6d537f11bc72ec23b426fea9f4fb5d02a0fe8e0
Saqib <saqib29111@gmail.com>
2020-11-11 19:40:45 +0600

    admin can fetch all users and can see their information except password

--- Lab_task/controllers/adminOperation.js
+++ Lab_task/controllers/adminOperation.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express           = require('express');
+const operation         = require.main.require('./models/operationModel');
 
-const router = express.Router();
+const router            = express.Router();
 
+router.get('*', (req, res, next) => {
+    if(req.session.username == null){
+        res.redirect('/login');
+    } else {
+        next();
+    }
+});
 
-
-
-
+router.get('/userlist', (req, res) => {
+    operation.getAll((result) => {
+        res.render('home/userlist', { users: result} );
+    });
+});
 
 module.exports = router;
