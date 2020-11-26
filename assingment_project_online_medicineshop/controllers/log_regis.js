c5a7d777f85ceb409c4117aa59da92cce4d6ce1b
Saqib <saqib29111@gmail.com>
2020-11-26 21:58:23 +0600

    login validation + database validation

--- assingment_project_online_medicineshop/controllers/log_regis.js
+++ assingment_project_online_medicineshop/controllers/log_regis.js
@@ -7,11 +7,40 @@ const router               = express.Router();
 
 // get pos route for login
 router.get('/login', (req, res) => {
-    res.render('home/login');
+    res.render('home/login', { username: 'undefined', password: 'undefined'});
 });
 
-router.post('/login', (req, res) => {
-
+router.post('/login', [
+        check('username', 'input valid username').not().isEmpty(),
+        check('password', 'password must be 6+ charachter').not().isEmpty().isLength({ min : 6})
+    ], (req, res) => {
+        var error = validationResult(req);
+        if(!error.isEmpty()) {
+            res.render('home/login', { username: (error.mapped().username) ? error.mapped().username : 'undefined', password: (error.mapped().password) ? error.mapped().password : 'undefined'});
+        } 
+        else {
+            var user = {
+                username: req.body.username,
+                password: req.body.password
+            };
+            user_db_connection.validate(user, (result) => {
+                if(result.length > 0){
+                    req.session.user = {
+                        id: result[0].id,
+                        username: result[0].username,
+                        password: result[0].password,
+                        contact: result[0].contact,
+                        user_roll: result[0].user_roll
+                    };
+                    
+                    res.send(req.session.user);
+                } else {
+                    res.send('user not existed, got to register');
+                }
+            });
+            // console.log(req.body);
+        }
+        // console.log(error.mapped());
 });
 
 // get post router for registration
--- assingment_project_online_medicineshop/models/user.js
+++ assingment_project_online_medicineshop/models/user.js
@@ -2,10 +2,10 @@ const db  =  require('./db');
 
 module.exports = {
     validate: (user, callback) => {
-        var sql = `SELECT * FROM user WHERE username = ${user.username} AND password = ${user.password}`;
-        db.getResult(sql, null, (result) => {
-            if(results.length > 0) {
-                callback(results);
+        var sql = `SELECT * FROM user WHERE username = '${user.username}' AND password = '${user.password}'`;
+        db.getResults(sql, null, (result) => {
+            if(result.length > 0) {
+                callback(result);
             } else {
                 callback(result);
             }
